49555fecafb988828b91a41f912b3e37d36802c7
htkddt <huynhtuankietddt@gmail.com>
2024-04-20 13:06:03 +0700

    Train model

--- YOLO_train.py
+++ YOLO_train.py
@@ -2,8 +2,9 @@ from ultralytics import YOLO
 
 # Load model
 # model = YOLO("yolov5nu.pt")
-model = YOLO("yolov8n.pt")
+# model = YOLO("yolov8n.pt")
+model = YOLO("yolov8n-seg.pt")
 
 if __name__ == '__main__':
     # Train model
-    results = model.train(data="mydataset.yaml", epochs=40, device="0", batch=2)
+    results = model.train(data="mydataset.yaml", epochs=20, device="0", batch=2)
