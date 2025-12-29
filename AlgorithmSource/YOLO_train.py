6eba562664e9a67af5d3e96aafa9ebfc34077138
htkddt <huynhtuankietddt@gmail.com>
2025-12-29 19:33:29 +0700

    Updated the path of YOLO model

--- AlgorithmSource/YOLO_train.py
+++ AlgorithmSource/YOLO_train.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 
 # Load model
 model = YOLO("../yolov8n.pt")
-# model = YOLO("D:/A_Project/PyCharm_Project/ApplicationRobotics/runs/detect/train/weights/best.pt")
+# model = YOLO("../runs/detect/train/weights/best.pt")
 # model = YOLO("../yolov8n-seg.pt")
-# model = YOLO("D:/A_Project/PyCharm_Project/ApplicationRobotics/runs/segment/train/weights/best.pt")
+# model = YOLO("../runs/segment/train/weights/best.pt")
 
 if __name__ == '__main__':
     # Train model
-    results = model.train(data="mydataset.yaml", epochs=30, device='cpu', batch=2)
+    results = model.train(data="mydataset.yaml", epochs=30, device='cpu', batch=4)
